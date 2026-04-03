eabe0d25b6529f45b96352b600b5dd77d4c2d8b3
abhiiiiiiiii18 <nischalabhi018@gmail.com>
2026-04-04 01:11:37 +0530

    feat: add cookie-parser and cors middleware, enhance error handling, and implement async handler

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,9 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+    const connectionInstance = await mongoose.connect(process.env.MONGODB_URL, {
+      dbName: DB_NAME
+    });
     console.log(`\nConnected to MongoDB successfully !! DB HOST: ${connectionInstance.connection.host}`);
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
--- src/index.js
+++ src/index.js
@@ -5,7 +5,14 @@ dotenv.config({
     path: "./.env"
 });
 
-connectDB().catch((error) => {
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+})
+.catch((error) => {
     console.error("Failed to connect to database:", error);
     process.exit(1);
 });
